023e94f9c9c8c72dc7bff6b105fd7653b3918a35
Suraj Samaga <surajsamaga@gmail.com>
2021-11-23 22:04:42 +0530

    Updated lime.py, and added LOE to helper.py

--- helper.py
+++ helper.py
@@ -22,9 +22,7 @@ def gamma_correct(imap, gamma=0.8):
     output - 
         asdf - M x N gamma corrected map
     """
-    # K = np.power(255, gamma)
     gamma_imap = np.power(imap, gamma)
-    # gamma_imap = gamma_imap*255
 
     return gamma_imap
 
@@ -38,18 +36,51 @@ def denoise(L, imap):
         Rf - M x N x 3 clear image
     """
     T = np.repeat(imap[:, :, np.newaxis], 3, axis=2)
-
-    # let entries be in (0,1)
-    L = L/255
-    T = T/255
-
     R = np.divide(L, T + 1e-5)
-    yuv_img = cv2.cvtColor(R, cv2.COLOR_BGR2YUV).astype(np.float32)
-    yd = bm3d.bm3d(yuv_img[:,:,0], sigma_psd=30/255, stage_arg=bm3d.BM3DStages.ALL_STAGES)
-    Rd = np.array(yuv_img)
-    Rd[:,:,0] = yd
-    Rd = cv2.cvtColor(Rd, cv2.COLOR_YUV2BGR).astype(np.float32)
+
+    # convert to yuv for denoising
+    r = R[:,:,2]
+    g = R[:,:,1]
+    b = R[:,:,0]
+
+    Y = 0.299*r + 0.587*g + 0.114*b
+    U = -0.14713*r - 0.28886*g + 0.436*b
+    V =0.615*r - 0.51499*g - 0.10001*b
+    yd = bm3d.bm3d(Y, sigma_psd=30/255, stage_arg=bm3d.BM3DStages.ALL_STAGES)
+
+    
+    # convert back to bgr
+    Rd = np.zeros((R.shape))
+    r = yd + 1.13983*V
+    g = yd - 0.39465*U - 0.5806*V
+    b = yd + 2.03211*U
+
+    Rd[:,:,0] = b
+    Rd[:,:,1] = g
+    Rd[:,:,2] = r
+
     #Recomposing
     Rf = L + np.multiply(Rd, (1-T))
-    # Rf= Rf*255
+    Rf= Rf*255
+    Rf = np.clip(Rf, 0, 255)
     return Rf
+
+def LOE(img, imgr):
+    """
+    inputs - 
+        img - M x N x 3 - input image
+        imgr - M x N x 3 - reconstructed image
+    outputs - 
+        loe - lightness order error
+    """
+    Q = np.amax(img, axis=2).reshape((img.shape[0]*img.shape[1]))
+    Qr = np.amax(imgr, axis=2).reshape((img.shape[0]*img.shape[1]))
+    loe = 0
+    for i in range(Q.shape[0]):
+        for j in range(Q.shape[0]):
+            Ul = 1 if Q[i]>=Q[j] else 0
+            Ur = 1 if Qr[i]>=Qr[j] else 0
+            tempsum = Ul^Ur
+            loe += tempsum
+    loe/=Q.shape[0]
+    return loe
--- lime.py
+++ lime.py
@@ -3,17 +3,26 @@ import cv2
 import numpy as np
 import argparse
 from helper import *
+import time
+
+def gradT(T, Dv, Dh):
+    return np.matmul(T,Dh), np.matmul(Dv,T)
 
 class LLenhance():
 
-    def __init__(self,args,alpha,gamma):
-        self.orig_img = cv2.imread(args.img_path).astype(np.float32)
+    def __init__(self,args):
+        self.orig_img = cv2.imread(args.img_path)
         if self.orig_img is None:
             raise ValueError("Invalid image path")
-            # self.orig_hsv = cv2.cvtColor(self.orig_img, cv2.COLOR_BGR2HSV)
-            # self.v = np.array(self.orig_hsv[:,:,2])
+        self.orig_img = self.orig_img/255
 
-    def fwdDiffToep(m):
+    def fwdDiffToep(self, m):
+        """
+        input -
+            m - size of array
+        output - 
+            D - discrete derivative operator
+        """
         D = np.zeros((m,m))
         for i in range(m-1):
             D[i,i] = -1
@@ -23,10 +32,15 @@ class LLenhance():
         D[m-1][m-1] = -1
         return D
 
-    def gradT(T):
-        return cv2.Sobel(T,cv2.CV_32F,1,0,ksize=3),cv2.Sobel(T,cv2.CV_32F,0,1,ksize=3)
-
-    def Tdenom(mu,m,n):
+    def Tdenom(self, mu,m,n):
+        """
+        computing denominator of T_{t+1} in its closed form updation solution
+        inputs - 
+            mu - positive scalar associated with the Z term (Largangian multiplier)
+            m,n - size of matrices
+        output - 
+            denom - denominator of T_{t+1}
+        """
         Dh = np.zeros((m,n))
         Dv = np.zeros((m,n))
         Dh[1,1] = -1
@@ -34,25 +48,36 @@ class LLenhance():
         Dv[1,1] = -1
         Dv[2,1] = 1
         Dhfft = np.fft.fft2(Dh)
-        Dvfft = np.fft.fft2(Dh)
+        Dvfft = np.fft.fft2(Dv)
         denom = 2 + mu*(Dhfft*np.conjugate(Dhfft) + Dvfft*np.conjugate(Dvfft))
         return denom
     
     def refineT(self,Tinit,alpha = 0.15,rho = 1.1):
-
-        m,n = Tinit.shape[0],Tinit[1]
+        """
+        implementing exact solver to refine initial illumination map 
+        inputs - 
+            Tinit - M * N initial illumination map
+            alpha - balance coefficient
+            rho - coefficient for mu update
+        outputs -
+            T - final illumination map
+        """
+        m,n = Tinit.shape[0],Tinit.shape[1]
         #Initialising G: gradT, Z: Lagrangian multiplier
         G = np.zeros((2*m,n))
         Z = np.zeros((2*m,n))
         #Initialising weight matrix using strategy II
-        delTih,delTiv = self.gradT(Tinit)
-        Wh = 1/(np.abs(delTih)+1e-5)
-        Wv = 1/(np.abs(delTiv)+1e-5)
-        W = np.concatenate(Wh,Wv)
+        Dv = self.fwdDiffToep(m)
+        Dh = self.fwdDiffToep(n).T
+        delTih,delTiv = gradT(Tinit, Dv, Dh)
+
+        Wh = 1/(np.abs(delTih)+1e-4)
+        Wv = 1/(np.abs(delTiv)+1e-4)
+        W = np.concatenate((Wh,Wv))
 
         mu = 0.05
         delta = 1e-5
-        max_iter = 100
+        max_iter = 10000
         #discrete gradient operators with forward difference
         Dv = self.fwdDiffToep(m)
         Dh = self.fwdDiffToep(n).T
@@ -63,10 +88,11 @@ class LLenhance():
             num = np.fft.fft2(2*Tinit + mu*(np.matmul(x[:m,:],Dh)+np.matmul(Dv,x[m:,:])))
             denom = self.Tdenom(mu,m,n)
             T = np.fft.ifft2(num/denom)
+            T = np.real(T)
 
             #G sub-problem
-            delTh,delTv = self.gradT(T)
-            delT = np.concatenate(delTh,delTv)
+            delTh,delTv = gradT(T, Dv, Dh)
+            delT = np.concatenate((delTh,delTv))
             epsilon = alpha*W/mu
             x = delT +Z/mu
             G = np.sign(x)*np.clip(np.abs(x)- epsilon,a_min = 0,a_max = None)
@@ -76,27 +102,27 @@ class LLenhance():
             mu = mu*rho
 
             #check convergence
-            if(np.linalg.norm(delT-G,'fro')< delta*np.linalg(Tinit,'fro')):
+            if(np.linalg.norm(delT-G,'fro')< delta*np.linalg.norm(Tinit,'fro')):
                 break
+            max_iter-=1
 
+        print("Number of iterations: ", max_iter)
         return T
 
     def enhanceLL(self,alpha,gamma):
         '''
         input: 
-            alpha
-            gamma
+            alpha - balance coefficient
+            gamma - gamma correction coefficient
         output:
-            Enhanced image
+            Rf - Enhanced image
         '''
         #Estimate initial illumination map
         Tinit = initial_map(self.orig_img)
-        Tinit /= 255
         #Refine illumination map
-        T = self.refineT(Tinit,alpha,rho = 1.1)
+        T = self.refineT(Tinit,alpha,rho = 2.0)
         #Gamma correction
         Tg = gamma_correct(T,gamma)
-        Tg = Tg*255
         #Denoising and Recomposing
         Rf = denoise(self.orig_img,Tg)
 
@@ -109,6 +135,10 @@ if __name__=="__main__":
     parser.add_argument('--alpha',type = float,default= 0.15,help='Parameter for refining illumination')
     parser.add_argument('--gamma',type = float,default= 0.8,help='Gamma to use for gamma correction')
     args = parser.parse_args()
+
+    start = time.time()
     enhancer = LLenhance(args)
     enhanced_img = enhancer.enhanceLL(args.alpha,args.gamma)
     cv2.imwrite(args.save_img, enhanced_img)
+    end = time.time()
+    print("Runtime:", end - start)
